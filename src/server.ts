f7cdab07ae9c893dd1a58e1123b6807bb135c8c0
Matvey <matvey150603@gmail.com>
2026-06-05 13:34:30 +0700

    feat: use custom http error heandler

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,26 @@
-import express from 'express';
+import express, {ErrorRequestHandler, NextFunction} from 'express';
 import {ENV} from './constants/env.js';
 import {invoiceRouter} from './modules/invoice/routes.js';
 import {merchantRepository} from './modules/merchant/repository.js';
+import {HttpError} from './utils/httpError.js';
 
 const app = express();
 
+const errorHandler: ErrorRequestHandler = (
+    err: HttpError,
+    req,
+    res,
+    next
+) => {
+    res.status(err.statusCode).json({
+        message: err.message,
+    });
+};
+
 app.use(express.json());
+
 app.use('/', invoiceRouter);
+app.use(errorHandler);
 
 
 export async function start() {
@@ -18,7 +32,7 @@ export async function start() {
         });
         console.log(`Test merchant ID: ${testMerchant.id}`);
     } else {
-         console.log(`Test merchant ID: ${merchants[0].id}`);
+        console.log(`Test merchant ID: ${merchants[0].id}`);
     }
 
     app.listen(ENV.PORT, () => {
